f95c89a20510b12abd7cafa6d5b1fb3376e8a303
florian-j-kuehn <florian.j.kuehn@gmail.com>
2017-04-17 15:28:05 +0200

    Created main function and docstring

--- create_py_file.py
+++ create_py_file.py
@@ -37,30 +37,35 @@ USAGESTR = """{versionstr}
         -r,--raw      : Show raw bytes repr() instead of trying to decode.
         -v,--version  : Show version.
 """.format(script=SCRIPT, versionstr=VERSIONSTR)
-#docopt(USAGESTR, version=VERSIONSTR)
 
-
-# Get (f)filename from cmdline.
-f_name = input('Input filename: ')
-f_file = f_name + '.py'
-
-
-# (d)Directory commands.
-d_name = input('Input new dir: ')
 
 
 # Create directory if needed. Throw error exception.
 # Wait two seconds before creation.
-try:
-    print('Creating directory {}'.format(d_name))
-    os.makedirs(d_name)
-except OSError as exception:
-    if exception.errno == errno.EEXIST:
-        sys.exit('There was an error creating the file.')
-        raise
+def main(argd):
+    # Get (f)filename from cmdline.
+    f_name = input('Input filename: ')
+    f_file = f_name + '.py'
 
 
-# Open file and write SHEBANG to it. Also print absolute file (p)path for new file.
-with open(os.path.join(d_name, f_file), 'w') as f:
-    f.write('#!/usr/local/bin/python3.6\n\n\n# Created by Florian Kuehn.')
-    print('File created ...' + os.path.abspath(os.path.join(d_name, f_file)))
+    # (d)Directory commands.
+    d_name = input('Input new dir: ')
+
+
+    """ Main entry point, expects doctopt arg dict as argd """
+    try:
+        print('Creating directory {}'.format(d_name))
+        os.makedirs(d_name)
+    except OSError as exception:
+        if exception.errno == errno.EEXIST:
+            sys.exit('There was an error creating the file.')
+            raise
+    # Open file and write SHEBANG to it. Also print absolute file (p)path for new file.
+    with open(os.path.join(d_name, f_file), 'w') as f:
+        f.write('#!/usr/local/bin/python3.6\n\n\n# Created by Florian Kuehn.')
+        print('File created ...' + os.path.abspath(os.path.join(d_name, f_file)))
+
+
+# Call the main function together with docstring if given.
+if __name__ == '__main__':
+    main(docopt(USAGESTR, version=VERSIONSTR))
---
+++ test/index.py
@@ -0,0 +1,4 @@
+#!/usr/local/bin/python3.6
+
+
+# Created by Florian Kuehn.
