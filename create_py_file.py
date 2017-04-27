efeddcbc15023b40137524f679239ef9b07463e0
florian-j-kuehn <florian.j.kuehn@gmail.com>
2017-04-27 21:20:32 +0200

    added a bigger header.

--- create_py_file.py
+++ create_py_file.py
@@ -2,14 +2,15 @@
 
 
 """
- Create new python file with minimum requirements.
+ Create new python file with SHEBANG.
  Created by Florian Kuehn.
 
  While creating the directory we try to catch an exception if something is
  wrong. Errors for already existing files are suppressed.
 
  Things to add:
-    - main function to make use of docopt
+    - main function to make use of docopt - done
+    - add docopt and main def stuff to file creation
 """
 
 
@@ -30,14 +31,48 @@ SCRIPTDIR = os.path.abspath(sys.path[0])
 USAGESTR = """{versionstr}
     Usage:
         {script} [-h | -v]
-        {script} -r
 
     Options:
         -h,--help     : Show this help message.
-        -r,--raw      : Show raw bytes repr() instead of trying to decode.
         -v,--version  : Show version.
 """.format(script=SCRIPT, versionstr=VERSIONSTR)
 
+
+
+# Header text starts here
+h = """
+#!/usr/local/bin/python3.6
+
+
+\"\"\"
+Create new python file with SHEBANG.
+Created by Florian Kuehn.
+
+\"\"\"
+
+
+from docopt import docopt
+
+
+NAME = 'xy'
+VERSION = '0.0.1'
+VERSIONSTR = '{} v. {}'.format(NAME, VERSION)
+SCRIPT = os.path.split(os.path.abspath(sys.argv[0]))[1]
+SCRIPTDIR = os.path.abspath(sys.path[0])
+USAGESTR = \"\"\"{versionstr}
+    Usage:
+        {script} [-h | -v]
+
+
+    Options:
+        -h,--help     : Show this help message.
+        -v,--version  : Show version.
+\"\"\".format(script=SCRIPT, versionstr=VERSIONSTR)
+
+
+def main(argd):
+# Start Programming here
+"""
 
 
 # Create directory if needed. Throw error exception.
@@ -62,7 +97,7 @@ def main(argd):
             raise
     # Open file and write SHEBANG to it. Also print absolute file (p)path for new file.
     with open(os.path.join(d_name, f_file), 'w') as f:
-        f.write('#!/usr/local/bin/python3.6\n\n\n# Created by Florian Kuehn.')
+        f.write(h)
         print('File created ...' + os.path.abspath(os.path.join(d_name, f_file)))
 
 
--- test/index.py
+++
@@ -1,4 +0,0 @@
-#!/usr/local/bin/python3.6
-
-
-# Created by Florian Kuehn.
